14899b1b485ddfb4cc79fc165532a2fa2273b25d
kiryachok <kiryachoka@gmail.com>
2015-02-24 11:15:30 +0300

    code refactor

--- EssModel/project.cpp
+++ EssModel/project.cpp
@@ -1,12 +1,29 @@
 #include "project.h"
 
-project::project()
+Project::Project()
 {
 
 }
 
-project::~project()
+Project::~Project()
 {
 
 }
 
+bool Project::saveFile(char* fileName)
+{
+    CArch *arh = new CArch(fileName, 1);
+    m_MapAttributes.Serialize(*arh);
+    m_MapFrames.Serialize(*arh);
+    m_MapProgModuls.Serialize(*arh);
+    m_MapStrategies.Serialize(*arh);
+}
+
+bool Project::loadFile(char *fileName)
+{
+    CArch *arh = new CArch(fileName, 1);
+    m_MapAttributes.Serialize(*arh);
+    m_MapFrames.Serialize(*arh);
+    m_MapProgModuls.Serialize(*arh);
+    m_MapStrategies.Serialize(*arh);
+}
--- EssModel/project.h
+++ EssModel/project.h
@@ -1,12 +1,36 @@
 #ifndef PROJECT_H
 #define PROJECT_H
+#include "QFile"
+#include "QString"
+#include "QDataStream"
+#include "QSaveFile"
+#include "objects.h"
+#include "archive.h"
 
-
-class project
+class Project
 {
 public:
-    project();
-    ~project();
+    Project();
+    ~Project();
+    bool saveFile(char* fileName);
+    bool loadFile(char* fileName);
+private:
+    CMapAttributes m_MapAttributes;
+    CMapProgModuls m_MapProgModuls;
+    CMapStrategies m_MapStrategies;
+    CMapFrames m_MapFrames;
+//    CArrayMenuESS m_ArrayMenu;
+    QString   m_Repository;
+
+
+    //---------------------------------------
+        int m_ActStrategy;
+        int m_ActProd;
+    //---------------------------------------
+        int m_ActFrame;
+        int m_ActSlot;
+        QString   m_ActModul;
+
 };
 
 #endif // PROJECT_H
